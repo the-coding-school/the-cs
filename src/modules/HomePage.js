a1450b106f92e53981cb6f3e30dff06206d29aab
Bibek <bibekg@ucla.edu>
2017-06-07 13:30:48 -0700

    Created IconPanel component

--- src/modules/HomePage.js
+++ src/modules/HomePage.js
@@ -4,8 +4,7 @@ import homepage_video from "../videos/homepage_video.mp4";
 import { TitledParagraphs, TitledContent, LinkButton, SectionHeader } from "./Skeleton";
 import dataJSON from "../json/data.json";
 import Testimonials from "./Testimonials";
-import { PartnersPanel } from "./Partners";
-import { SchoolsPanel } from "./Schools";
+import IconPanel from "./IconPanel";
 import '../scss/Home.scss';
 import videoPoster from "../images/video_poster.jpg";
 import $ from 'jquery';
@@ -14,7 +13,29 @@ class HomePage extends Component {
   render() {
     const miP = dataJSON.paragraphs.mainIntro;
 
-    const bringcodingLink = <LinkButton text="Bring coding to your school!"> <Link to={"/sign-up"} /> </LinkButton>
+    function sortSchools(a, b) {
+      if (a.name === "Santa Monica-Malibu USD") return -1;
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    }
+
+    function sortPartners(a, b) {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    }
+
+    const sortedPartners = dataJSON.partners.sort(sortPartners)
+    sortedPartners.forEach(function(i){
+      i.imageURL = process.env.PUBLIC_URL + "images/partners/" + i.imageName;
+    });
+
+    const sortedSchools = dataJSON.schools.sort(sortSchools)
+    sortedSchools.forEach(function(i){
+      i.imageURL = process.env.PUBLIC_URL + "/images/schools/" + i.imageName;
+    });
+
 
     return (
       <div className="App">
@@ -23,19 +44,26 @@ class HomePage extends Component {
           <IntroBlurb />
           <Testimonials />
           <div className="homepage_panels">
+
             <div className="homepage_panel_wrapper">
               <div className="homepage_panel">
                 <h1 className="centered_header">Our Partners</h1>
-                <PartnersPanel />
+                <div className="partners_panel">
+                  <IconPanel items={sortedPartners} />
+                </div>
                 <p>In addition, The Coding School is proud to have been awarded the Brown Venture Launch Fund from Brown University Social Innovation Initiative.</p>
               </div>
             </div>
+
             <div className="homepage_panel_wrapper">
               <div className="homepage_panel">
                 <h1 className="centered_header">School Districts We Have Coding Programs In</h1>
-                <SchoolsPanel />
+                <div className="schools_panel">
+                  <IconPanel items={sortedSchools} />
+                </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
