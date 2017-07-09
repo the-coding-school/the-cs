9337aa29d0f78ec7bfa1d61ce05ae48f73c7f59d
Bibek Ghimire <bibek@stanza.co>
2017-07-09 10:47:12 -0700

    first commit

--- src/components/TitledContent/TitledContent.js
+++ src/components/TitledContent/TitledContent.js
@@ -5,13 +5,15 @@ import './TitledContent.scss';
 export default class TitledContent extends React.Component {
   render() {
     return (
-      <div className='titled_content' key={this.props.title}>
-        <div className='title_side'>
-          <h1 className='title'>{this.props.title}</h1>
-          { ('subtitle' in this.props) && <h2 className='subtitle'>{this.props.subtitle}</h2> }
-        </div>
-        <div className='content_side'>
-          {this.props.content}
+      <div className='titled_content_wrapper'>
+        <div className='titled_content' key={this.props.title}>
+          <div className='title_side'>
+            <h1 className='title'>{this.props.title}</h1>
+            { ('subtitle' in this.props) && <h2 className='subtitle'>{this.props.subtitle}</h2> }
+          </div>
+          <div className='content_side'>
+            {this.props.content}
+          </div>
         </div>
       </div>
     );
--- src/containers/DonationsPanel/DonationsPanel.js
+++ src/containers/DonationsPanel/DonationsPanel.js
@@ -21,33 +21,35 @@ export default class DonationsPanel extends React.Component {
     const TAX_MESSAGE = 'The Coding School is a 501c3 organization. Your gift is tax deductible to the extent allowed by the law. When you make a donation through PayPal, you will automatically receive a receipt that can be used for tax deduction purposes.  Our public charity number is 46-5097610.';
 
     return (
-      <div className='donations_panel'>
-        <div className='title'>Donate</div>
-        <div className='message'>{DESCRIPTION}</div>
-        <div className='donations_options'>
-          <DonationOption
-            amount={child.amount}
-            category={child.category}
-            imageSrc={fundChildImage}
-            buttonID={child.buttonID}
-          />
+      <div className='donations_panel_wrapper'>
+        <div className='donations_panel'>
+          <div className='title'>Donate</div>
+          <div className='message'>{DESCRIPTION}</div>
+          <div className='donations_options'>
+            <DonationOption
+              amount={child.amount}
+              category={child.category}
+              imageSrc={fundChildImage}
+              buttonID={child.buttonID}
+            />
 
-          <DonationOption
-            amount={classroom.amount}
-            category={classroom.category}
-            imageSrc={fundClassroomImage}
-            buttonID={classroom.buttonID}
-          />
+            <DonationOption
+              amount={classroom.amount}
+              category={classroom.category}
+              imageSrc={fundClassroomImage}
+              buttonID={classroom.buttonID}
+            />
 
-          <DonationOption
-            amount={school.amount}
-            category={school.category}
-            imageSrc={fundSchoolImage}
-            buttonID={school.buttonID}
-          />
+            <DonationOption
+              amount={school.amount}
+              category={school.category}
+              imageSrc={fundSchoolImage}
+              buttonID={school.buttonID}
+            />
+          </div>
+          <DonateCustom />
+          <div className='tax_message'>{TAX_MESSAGE}</div>
         </div>
-        <DonateCustom />
-        <div className='tax_message'>{TAX_MESSAGE}</div>
       </div>
     );
   }
