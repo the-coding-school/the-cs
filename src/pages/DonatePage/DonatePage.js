15338c822e6f753df4277fe087d47b8d3d6262f8
Bibek Ghimire <bibek@stanza.co>
2017-07-10 08:55:21 -0700

    Set up fields for 3 main forms

--- src/pages/DonatePage/DonatePage.js
+++ src/pages/DonatePage/DonatePage.js
@@ -16,8 +16,6 @@ export default class DonatePage extends React.Component {
       volunteer
     } = pageData;
 
-    // TODO: Add form links for all sections once created
-
     return (
       <div className='App'>
         <PageHeader image={headerImage} title={header.title} description={header.description} />
