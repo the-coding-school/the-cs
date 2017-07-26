36f990e912d26377d626f8d4cbe6eaf8c12cdf73
Bibek <bibekg@ucla.edu>
2017-07-26 08:46:49 -0700

    Update Team numbers

--- src/pages/TeamPage/TeamPageData.js
+++ src/pages/TeamPage/TeamPageData.js
@@ -2,6 +2,6 @@ export default {
   header:{
     image: require('./header.jpg'),
     title: "Our Team",
-    description: "Our team is comprised of 500+ of the most passionate computer science university students and professional software engineers from around the United States. From over 40 universities and 20 companies, our teachers study at MIT, UCLA, Ole Miss, and Duke; work at Bain, IBM, SpaceX and Facebook; and have served in the military, written code for life-saving procedures used by poison control centers, and sent satellites up into space."
+    description: "Our team is comprised of 500+ of the most passionate computer science university students and professional software engineers from around the United States. From over 75 universities and 30 companies, our teachers study at MIT, UCLA, Ole Miss, and Duke; work at Bain, IBM, SpaceX and Facebook; and have served in the military, written code for life-saving procedures used by poison control centers, and sent satellites up into space."
   }
 }
