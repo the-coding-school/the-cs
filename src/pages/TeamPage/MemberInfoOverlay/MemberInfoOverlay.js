e0d02bf143992761ae08c2862f02b54f71afd929
Bibek <bibekg@ucla.edu>
2017-07-28 09:08:21 -0700

    Info overlay only comes up if there's a description

--- src/pages/TeamPage/MemberInfoOverlay/MemberInfoOverlay.js
+++ src/pages/TeamPage/MemberInfoOverlay/MemberInfoOverlay.js
@@ -10,14 +10,14 @@ export default class MemberInfoOverlay extends React.Component {
   render() {
     const p = this.props
     return (
-      <div className='member_info_overlay'>
+      <div className={`member_info_overlay ${p.description && 'hoverUp'}`}>
         <div className='member_main_info'>
           <MemberName name={p.name} />
           <MemberPosition position={p.position}/>
         </div>
-        <div className='member_secondary_info'>
+        {p.description && <div className='member_secondary_info'>
           <MemberDescription description={p.description} />
-        </div>
+        </div>}
       </div>
     )
   }
