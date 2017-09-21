d9225eec463285d125908e452d5b7e25fcb6aaba
Bibek Ghimire <ghim.bibek@gmail.com>
2017-09-21 11:28:38 -0700

    IS-68 - Add a NoMatch route (#69)
    * Add a NoMatch route
    
    * Whoops, add unstaged files
    
    * Modify deploy script for switch over to S3
    
    * Clean up index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,29 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from 'views/Navbar';
 import Footer from 'views/Footer';
+import NoMatch from 'pages/NoMatch';
 import pages from 'pages';
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
       <Navbar />
-      {
-        pages.map(p => {
-          const routeProps = {
-            key: p.path,
-            path: p.path,
-            component: p.component,
-            exact: Boolean(p.exactPath)
-          };
-
-          return <Route {...routeProps} />
-        })
-      }
+      <Switch>
+        {
+          pages.map(page => (
+            <Route
+              key={page.path}
+              path={page.path}
+              component={page.component}
+              exact={Boolean(page.exactPath)}
+            />
+          ))
+        }
+        <Route component={NoMatch} status={400} />
+      </Switch>
       <Footer />
     </div>
   </BrowserRouter>,
