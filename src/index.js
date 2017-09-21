4e327c0f8b7a32d7f9fc642a4dd6ede06a121a40
Bibek <bibekg@ucla.edu>
2017-09-21 11:10:01 -0700

    Add a NoMatch route

--- src/index.js
+++ src/index.js
@@ -2,27 +2,31 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from 'views/Navbar';
 import Footer from 'views/Footer';
+import NoMatch from 'pages/NoMatch';
 import pages from 'pages';
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
       <Navbar />
-      {
-        pages.map(p => {
-          const routeProps = {
-            key: p.path,
-            path: p.path,
-            component: p.component,
-            exact: Boolean(p.exactPath)
-          };
+      <Switch>
+        {
+          pages.map(p => {
+            const routeProps = {
+              key: p.path,
+              path: p.path,
+              component: p.component,
+              exact: Boolean(p.exactPath)
+            };
 
-          return <Route {...routeProps} />
-        })
-      }
+            return <Route {...routeProps} />
+          })
+        }
+        <Route component={NoMatch} status={400} />
+      </Switch>
       <Footer />
     </div>
   </BrowserRouter>,
