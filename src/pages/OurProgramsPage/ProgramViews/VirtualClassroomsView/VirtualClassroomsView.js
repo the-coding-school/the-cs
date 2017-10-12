0d66213a5c052210a38ef25012212b78ed974b0a
Kathy Nguyen <kathymn@gwu.edu>
2017-10-11 21:20:24 -0400

    Increased height of Program Footer

--- src/pages/OurProgramsPage/ProgramViews/VirtualClassroomsView/VirtualClassroomsView.js
+++ src/pages/OurProgramsPage/ProgramViews/VirtualClassroomsView/VirtualClassroomsView.js
@@ -3,13 +3,16 @@
 import React from 'react';
 import VirtualClassroomsPanel from 'views/VirtualClassroomsPanel';
 import ProgramFooter from 'components/ProgramFooter';
+import viewData from './VirtualClassroomsViewData';
 import './VirtualClassroomsView.scss';
 
 export default function VirtualClassroomsView() {
+  const { footer } = viewData;
+
   return (
     <div className='virtual_classrooms_view'>
       <VirtualClassroomsPanel />
-      <ProgramFooter image={require('./header.jpg')} title={'With our video chat and collaborative editing platform, we can bring our instructors and year-long courses into entire classrooms. This is especially beneficial for schools located in rural communities.'}/>
+      <ProgramFooter image={footer.image} title={footer.title}/>
     </div>
   );
 }
