e56889d495c23e283a1ce2dec6aa7ff55acd77b4
Kathy Nguyen <kathymn@gwu.edu>
2017-10-11 21:20:24 -0400

    Increased height of Program Footer

---
+++ src/pages/OurProgramsPage/ProgramViews/VirtualClassroomsView/VirtualClassroomsViewData.js
@@ -0,0 +1,8 @@
+// @flow
+
+export default {
+  footer: {
+    image: require('./footer.jpg'),
+    title: 'With our video chat and collaborative editing platform, we can bring our instructors and year-long courses into entire classrooms. This is especially beneficial for schools located in rural communities.'
+  }
+};
