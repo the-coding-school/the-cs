d9d1a4c34a3df555142c03dc5295eab87d09bac2
Kathy Nguyen <kathymn@gwu.edu>
2017-11-09 15:55:34 -0500

    Resized and removed duplicate VirtualClassrooms footer pic

--- src/pages/OurProgramsPage/ProgramViews/VirtualClassroomsView/VirtualClassroomsViewData.js
+++ src/pages/OurProgramsPage/ProgramViews/VirtualClassroomsView/VirtualClassroomsViewData.js
@@ -2,7 +2,7 @@
 
 export default {
   footer: {
-    image: require('./footer.jpg'),
+    image: require('images/programs-page/virtual-learning.jpg'),
     title: 'With our video chat and collaborative editing platform, we can bring our instructors and year-long courses into entire classrooms. This is especially beneficial for schools located in rural communities.'
   }
 };
