b94bb4e3bb07d30b2667813d3ff3f1195cfc01a9
Zachary Sang <zacharysang@gmail.com>
2017-12-10 23:04:12 -0500

    Updated component to use svg instead of image

--- src/components/ProgramElementIcon/ProgramElementIcon.js
+++ src/components/ProgramElementIcon/ProgramElementIcon.js
@@ -1,17 +1,21 @@
+// @flow
+
 import './ProgramElementIcon.scss';
 import React from 'react';
 
 export default class ProgramElementIcon extends React.Component {
     render () {
         const {
-            iconSrc,
-            desc
+		icon,
+		desc
         } = this.props;
 
         return (
 
             <div className="program_element_icon">
-                <img className="element_icon" src={iconSrc} alt={desc}/>
+                <div className="element_icon">
+			{icon}
+		</div>
                 <div className="element_description">
                 {desc}
                 </div>
