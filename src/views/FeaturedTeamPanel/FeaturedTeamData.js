d5ea4c0b8fd098b20ae2d10384a35eec625d9673
zacharysang <zacharysang@gmail.com>
2017-12-18 13:44:51 +0000

    Work done for #40 to add featured team members panel to home page

---
+++ src/views/FeaturedTeamPanel/FeaturedTeamData.js
@@ -0,0 +1,68 @@
+export default {
+    spiel: 'We have the largest network of computer scientists in the country, all passionate about teaching coding',
+    stats: [
+        {title: 'people', figure: '500+'}
+        , {title: 'countries', figure: '65'}
+        , {title: 'companies', figure: '30'}
+        , {title: 'universities', figure: '75'}
+    ],
+    teamMembers: [
+        {
+            name: 'kiera',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },
+        {
+            name: 'kiera1',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },{
+            name: 'kiera2',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },{
+            name: 'kiera3',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },{
+            name: 'kiera4',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },{
+            name: 'kiera5',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },{
+            name: 'kiera6',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },{
+            name: 'kiera7',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        },{
+            name: 'kiera8',
+            images: {
+                regular: require('images/team/kiera_peltz.jpg'),
+                fun: require('images/team/kiera_peltz_fun.jpg')
+            }
+        }
+        
+    ]
+}
---
+++ src/views/FeaturedTeamPanel/FeaturedTeamPanel.js
@@ -0,0 +1,47 @@
+import './FeaturedTeamPanel.scss';
+
+import React from 'react';
+import MemberImage from 'components/MemberImage';
+import StatBlock from 'components/StatBlock';
+
+import panelData from './FeaturedTeamData';
+
+export default class FeaturedTeamPanel extends React.Component {
+  render() {
+    return (
+      <div className='featuredTeamPanel'>
+        <div className='imageGrid'>
+            {
+                panelData.teamMembers.map( (member)=>{
+                    return (
+                        <div key={member.name} className='memberImage'>
+                            <MemberImage isSquare={true} images={member.images}/>
+                        </div>
+                    );
+                } )
+                
+            }
+        </div>
+        <div className='info'>
+            <div className='spiel'>
+                {panelData.spiel}
+            </div>
+            <div className='statsGrid'>
+                {panelData.stats.map( (stat)=> {
+                    return (
+                        
+                        <StatBlock key={stat.title} title={stat.title} figure={stat.figure}/>
+                        
+                    );
+
+                
+                })}
+            </div>
+            <a className='button' href='/team'>
+                Meet our incredible team
+            </a>
+        </div>
+      </div>
+    );
+  }
+}
