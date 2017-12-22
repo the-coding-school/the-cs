e1ca6b35ab293aa8e9d90867bbe718049ca91c26
Kathy Nguyen <kathymn@gwu.edu>
2017-12-22 01:23:47 -0500

    Made rows different colors

--- src/pages/OurProgramsPage/ProgramViews/LearningDisabilitiesView/LearningDisabilitiesPanel/LearningDisabilitiesPanel.js
+++ src/pages/OurProgramsPage/ProgramViews/LearningDisabilitiesView/LearningDisabilitiesPanel/LearningDisabilitiesPanel.js
@@ -10,11 +10,27 @@ export default class LearningDisabilitiesPanel extends React.Component {
     const statsIconRoot = process.env.PUBLIC_URL + '/images/learningDisabilities/';
     return (
       <div className='learning_disabilities_panel'>
-        {
-          learningDisabilities.map(function(s) {
-            return <LearningDisabilities key={s.description} imageSrc={statsIconRoot + s.imageName} description={s.description} display={s.display} />
-          })
-        }
+        <div className='row_1'>
+          {
+            learningDisabilities.row1.map(function(s) {
+              return <LearningDisabilities key={s.description} imageSrc={statsIconRoot + s.imageName} description={s.description} display={s.display} />
+            })
+          }
+        </div>
+        <div className='row_2'>
+          {
+            learningDisabilities.row2.map(function(s) {
+              return <LearningDisabilities key={s.description} imageSrc={statsIconRoot + s.imageName} description={s.description} display={s.display} />
+            })
+          }
+        </div>
+        <div className='row_3'>
+          {
+            learningDisabilities.row3.map(function(s) {
+              return <LearningDisabilities key={s.description} imageSrc={statsIconRoot + s.imageName} description={s.description} display={s.display} />
+            })
+          }
+        </div>
       </div>
     )
   }
